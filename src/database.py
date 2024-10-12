54d015b2e1601aa15d408a5400dfdd5bf77a7bf4
sourabh tiwari <sourabh7iwari@gmail.com>
2024-10-12 16:57:00 +0530

    added dotenv to process the env var perfectly

--- src/database.py
+++ src/database.py
@@ -1,11 +1,15 @@
 import psycopg2
 import os
+from dotenv import load_dotenv
+load_dotenv()  # This should be at the top of your main application file
+
 
 def connect_db():
+    password = os.getenv("DB_PASSWORD")  # Ensure this prints the expected password
     conn = psycopg2.connect(
         dbname="mydatabase",
         user="sourabh",
-        password=os.getenv("DB_PASSWORD"),
+        password=password,
         host="localhost",
         port="5432"
     )
